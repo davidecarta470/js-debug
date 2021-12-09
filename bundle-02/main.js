0cb8d200306409ffd5efd5b71f2b0696c24ee009
davidecarta470 <cartadavi@gmail.com>
2021-12-09 17:03:43 +0100

    second bundle

--- bundle-02/main.js
+++ bundle-02/main.js
@@ -13,43 +13,47 @@
 // ESERCIZIO 1
 function checkAge() {
     const myAge = 32;
-    const message = '';
+    let message = '';
 
     if (myAge < 18) {
         message = `Sei troppo giovane! Hai ${myAge} anni!`;
     } else {
         message = 'Hai più di 18 anni!';
     }
+    console.log(message)
 }
 checkAge();
+// il codice verifica che il valore immesso in myAge sia minore di 18...in relazione a questo assegna ad una variabile message 
+// una stringa.Non ha un valore di ritorno.Non sono presenti errori di sintassi.Errori logici sono che message all'interno della funzione  è  una variabile
+
 
 // ESERCIZIO 2
 function printColorsNumber() {
     const colors = ['blue', 'red', 'yellow', 'green', 'black'];
-    console.log(`Nella mia palette ci sono ${colors.lenght} colori!`);
+    console.log(`Nella mia palette ci sono ${colors.length} colori!`);
 }
 printColorsNumber();
-
+// il codice utilizza  la lunghezza dell'array per dare informazioni.Errori di sintassi : length era scritto male
 
 // ESERCIZIO 3
 function addNumbers() {
     const userNumber = prompt('Inserisci un numero');
-    const total = userNumber + 12;
+    const total = parseInt(userNumber) + 12;
 
     console.log(`Il risultato finale è ${total}`);
 }
 addNumbers();
-
+// il codice dovrebbe restituire la somma di due numeri.Errori logici: manca la funzione parsInt() che converte un numero inserito come stringa in valore numerico
 
 // ESERCIZIO 4
 function checkAccess() {
     const addresses = ['mymail@mail.com', 'yourmail@mail.com', 'hermail@mail.com', 'hismail@mail.com'];
     const userEmail = prompt('Inserisci il tuo indirizzo email');
 
-    let grantAccess = 'false';
+    let grantAccess = false;
 
     if (addresses.includes(userEmail)) {
-        grantAccess = 'true';
+        grantAccess = true;
     }
 
     if (grantAccess === true) {
@@ -59,9 +63,9 @@ function checkAccess() {
     }
 }
 checkAccess();
+// I valori booleani devono essere assegnati senza utilizzare gli apici
 
-
-// ESERCIZIO 5 (suggerimento: c'è un solo errore)
+// // ESERCIZIO 5 (suggerimento: c'è un solo errore)
 function checkAccessImproved() {
     const addresses = ['mymail@mail.com', 'yourmail@mail.com', 'hermail@mail.com', 'hismail@mail.com'];
 
@@ -75,12 +79,13 @@ function checkAccessImproved() {
       if (userEmail.length > 5) {
             
         if (email === userEmail) {
-        grantAccess = 'true';            
+        grantAccess = true;            
             
       }
         
     }
 
+    }
     if (grantAccess) {
         console.log('Accesso consentito!');
     } else {
@@ -89,10 +94,10 @@ function checkAccessImproved() {
 }
 checkAccessImproved();
 
+// come esercizio precedente verifica che all'interno di un array ci sia un determinato valore (in questo caso una stringa) 
+// è presente un ulteriore controllo... verifica che la stringa immessa non abbia meno di 5 caratteri.
 
-
-
-
+// errori di sintassi : mancava una parentesi graffa(quella della funzione) ; la condizione if(grantAccess){}else{} va posta fuori dal ciclo for
 
 
 
